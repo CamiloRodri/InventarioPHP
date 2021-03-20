2b7357aa8c9c283cddd4ddf93f6e9ef241325593
Camilo Rodriguez <camilorodri28@outlook.com>
2021-03-20 16:26:40 -0500

    Favicon agregado

--- Views/Includes/head.php
+++ Views/Includes/head.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" type="image/jpg" href="../../public/assets/img/favicon.ico"/>
 <meta charset="utf-8" />
 <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
 <link rel="icon" type="image/png" href="../assets/img/favicon.png">
