63214fcab9fd4fd38689acb10e9f7393e150f2d7
Camilo Rodriguez <camilorodri28@outlook.com>
2021-03-20 16:28:56 -0500

    Enlaces de Inicio

--- Views/Includes/sidebar.php
+++ Views/Includes/sidebar.php
@@ -1,12 +1,12 @@
 <div class="sidebar" data-color="white" data-active-color="danger">
       <div class="logo">
-        <a href="https://www.creative-tim.com" class="simple-text logo-mini">
+        <a href="../../index.php" class="simple-text logo-mini">
           <div class="logo-image-small">
             <img src="../../public/assets/img/logo-small.png">
           </div>
           <!-- <p>CT</p> -->
         </a>
-        <a href="https://www.creative-tim.com" class="simple-text logo-normal">
+        <a href="../../index.php" class="simple-text logo-normal">
           Nombre Empresa
           <!-- <div class="logo-image-big">
             <img src="../assets/img/logo-big.png">
